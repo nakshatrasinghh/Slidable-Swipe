72180652851f144e2ac85c662da10c80ca3fbdf1
nakshatrasinghh <nakshatradsu@gmail.com>
2021-09-08 09:03:54 +0530

    init

---
+++ android/app/src/main/kotlin/com/example/slideable/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.slideable
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/data/data.dart
@@ -0,0 +1,31 @@
+import 'package:slideable/model/chat.dart';
+
+class Data {
+  static const chats = <Chat>[
+    Chat(
+        username: 'Trevor',
+        message: 'Hey, how\'s it going? What did you do today?',
+        urlAvatar:
+            'https://images.unsplash.com/photo-1529626455594-4ff0802cfb7e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=668&q=80'),
+    Chat(
+        username: 'Katti',
+        message: 'How are you mac?',
+        urlAvatar:
+            'https://images.unsplash.com/photo-1524504388940-b1c1722653e1?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fHBlb3BsZXxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=900&q=60'),
+    Chat(
+        username: 'Johnson',
+        message: 'Wanna go out to eat some food?',
+        urlAvatar:
+            'https://images.unsplash.com/photo-1501196354995-cbb51c65aaea?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTd8fHBlb3BsZXxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=900&q=60'),
+    Chat(
+        username: 'Cavo',
+        message: 'Connect at 4pm.',
+        urlAvatar:
+            'https://images.unsplash.com/photo-1463453091185-61582044d556?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTl8fHBlb3BsZXxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=900&q=60'),
+    Chat(
+        username: 'Bruce',
+        message: 'Fight me!',
+        urlAvatar:
+            'https://images.unsplash.com/photo-1533227268428-f9ed0900fb3b?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTh8fHBlb3BsZXxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=900&q=60'),
+  ];
+}
